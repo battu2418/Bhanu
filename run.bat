d693d0cb34736bfa4d0f0b0e4f59a24f127b9728
battu2418 <80304482+battu2418@users.noreply.github.com>
2022-01-31 16:26:27 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 cd C:\Users\Bhanu.Battu\PycharmProjects\TestProject
 REM robot --outputdir Results TestCases/Administration
-robot --outputdir Results TestCases/Administration/Search_Userrole.robot
-REM robot --outputdir Results TestCases/Administration/Admin-ManageAwards.robot
+REM robot --outputdir Results TestCases/Administration/Search_Userrole.robot
+robot --outputdir Results TestCases/Administration/Admin-ManageAwards.robot
 
 
